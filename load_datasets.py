ebf58e2539ee5382c7eec8569b774181e8a01ddc
YoojLee <daebaq27@gmail.com>
2023-06-06 04:50:06 +0000

    Changed data path

--- load_datasets.py
+++ load_datasets.py
@@ -1,9 +1,9 @@
 from detectron2.data.datasets import register_coco_instances
 
 def load_datasets():
-    voc_image_path = '/fs/vulcan-datasets/VOCdevkit/VOC2007/JPEGImages'
-    coco_image_path = '/fs/vulcan-datasets/coco/images/train2017/'
-    coco_val_image_path = '/fs/vulcan-datasets/coco/images/val2017'
+    voc_image_path = 'voc2007/VOCdevkit/VOC2007/JPEGImages'
+    coco_image_path = '/home/work/yujin/data/train2014/'
+    coco_val_image_path = '/home/work/yujin/data/coco2017/val2017'
 
     pascal_voc_2007_trainval = "./data_jsons/voc_trainval_2007.json"
     register_coco_instances("custom_pascal_voc_trainval_2007", {},
